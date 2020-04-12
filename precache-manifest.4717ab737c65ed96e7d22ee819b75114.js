c2cbef73b2c15029b00c694dd8dfc4b55a239dc0
lpilas24 <leiva20152@gmail.com>
2020-04-11 21:42:44 -0500

    Updates

---
+++ precache-manifest.4717ab737c65ed96e7d22ee819b75114.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c3655fdacfdfccc2911c650bf54dc7",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "320e104c04e4528d75d0",
+    "url": "/corona-tracker/static/css/main.ed6725ac.chunk.css"
+  },
+  {
+    "revision": "ee86772928ffc25bed30",
+    "url": "/corona-tracker/static/js/2.45a24b2f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.45a24b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "320e104c04e4528d75d0",
+    "url": "/corona-tracker/static/js/main.2416c167.chunk.js"
+  },
+  {
+    "revision": "ca2958e8a11c11fa9939",
+    "url": "/corona-tracker/static/js/runtime-main.db1d68cc.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
